09bf28144a99c521049aab9904f11ead0aca4617
yhanca96 <yhann.423@hotmail.com>
2025-04-23 19:54:50 -0500

    creación de LoginRequest y JwtManager Helper

---
+++ Servicios.Parcial3/Helpers/JwtManager.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Web;
+
+namespace Servicios.Parcial3.Helpers
+{
+    public class JwtManager
+    {
+        private const string SecretKey = "ClaveSuperSecreta123456"; // Usa la misma en todo
+        private static readonly byte[] SecretKeyBytes = Encoding.UTF8.GetBytes(SecretKey);
+
+        public static string GenerateToken(string username, int expireMinutes = 60)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = SecretKeyBytes;
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name, username)
+                }),
+                Expires = DateTime.UtcNow.AddMinutes(expireMinutes),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+
+        public static TokenValidationParameters GetValidationParameters()
+        {
+            return new TokenValidationParameters
+            {
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(SecretKeyBytes),
+                ClockSkew = TimeSpan.Zero
+            };
+        }
+    }
+}
---
+++ Servicios.Parcial3/Models/LoginRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Servicios.Parcial3.Models
+{
+    public class LoginRequest
+    {
+        public string Usuario { get; set; }
+        public string Clave { get; set; }
+    }
+
+}
